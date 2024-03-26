7db1ad50adb786c0246ce5a7cb1bcbc05cf73e39
geod <geodgit@duck.com>
2024-03-26 16:41:55 +0100

    Add "Join us" modal and the "Support us" page

---
+++ src/Controller/SupportController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SupportController extends AbstractController
+{
+    #[Route('/accompagner-le-projet', name: 'app_support')]
+    public function index(): Response
+    {
+        return $this->render('support/index.html.twig');
+    }
+}
